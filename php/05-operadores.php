3b842224f1ba8a060d92d734d16fb29f22c68f19
iespenanovo <correo.iaw@gmail.com>
2021-10-05 12:36:26 +0200

    c├│digo iaw

--- php/05-operadores.php
+++ php/05-operadores.php
@@ -33,7 +33,98 @@ echo "<br>\$r c/b "; var_dump($r);
 $r=$c%2;
 echo "<br>\$r c%2 "; var_dump($r);
 
+echo "\n<hr>Operadores de asignación<hr>";
 
+//Operadores de asignación =  += -= *= /= .=
+
+// o operador = de asignación 'reescribe' o contido da variable que ten a esquerda
+
+$a=10;
+$a=12; //se perde o valor anterior de $a
+echo "<br>\$a "; var_dump($a);
+//operador += suma o resultado á variable da esquerda
+$a+=2; // equivalente $a=$a+2;
+echo "<br>\$a "; var_dump($a);
+
+$a-=1; // equivalente $a=$a-1 , equivalente a $a--;
+echo "<br>\$a "; var_dump($a);
+
+$a*=2; // equivalente $a=$a*2;
+echo "<br>\$a "; var_dump($a);
+
+$a/=2; // equivalente $a=$a/2;
+echo "<br>\$a "; var_dump($a);
+
+$nome="Ana";
+$nome.=" Fernández"; //equivalente $nome=$nome." Fernández";
+echo "<br>\$a "; var_dump($nome);
+ // operadores de comparación ==, !=, <, >, <= y >=
+
+if ($a==13) {
+	echo "<br>\$a ten valor 13 ($a)";
+}
+if ($a!=100) {
+	echo "<br>\$a non ten valor 100 ($a)";
+}
+
+if ($a>$c) {
+	echo "<br>\$a ($a) é maior que \$c ($c) ";
+}
+
+if ($a<=$c) {
+	echo "<br>\$a ($a) é menor ou igual que \$c ($c) ";	
+} else {
+	echo "<br>\$a ($a) non é menor ou igual que \$c ($c) ";	
+}
+
+echo "<br> a condición \$a==13 é ";
+echo $a==13;
+
+echo ($a==13) ? "<br>\$a vale 13 ($a)" : "<br>\$a non vale 13 ($a)";
+$a=10;
+echo ($a==13) ? "<br>\$a vale 13 ($a)" : "<br>\$a non vale 13 ($a)";
+
+//Operadores de incremento y decremento ++ e --
+//incrementan o decrementan nunha unidade a varible que se lle aplica
+
+$a++;
+echo "<br>o valor de \$a despois de facer un \$a++ é <b>$a</b>";
+$a++;
+echo "<br>o valor de \$a despois de facer un \$a++ é <b>$a</b>";
+$a--;
+echo "<br>o valor de \$a despois de facer un \$a-- é <b>$a</b>";
+
+echo "<br>O valor de \$a é <b>$a</b>";
+$resultado = 10 + $a++; //equivalente a : $resultado = 10 + $a; $a++;
+echo "<br>O valor de \$resultado despois de \$resultado = 10 + \$a++ é <b>$resultado</b>";
+echo "<br>O valor de \$a é <b>$a</b>";
+
+$a=11;
+echo "<br>O valor de \$a é <b>$a</b>";
+$resultado = 10 + ++$a; //equivalente a : $a++; $resultado = 10 + $a;
+echo "<br>O valor de \$resultado despois de \$resultado = 10 + ++\$a é <b>$resultado</b>";
+echo "<br>O valor de \$a é <b>$a</b>";
+
+// ollo, se ++ ou -- vai pola esquerda da variable, ten prioridade antes do resto de operacións
+// se vai pola dereita, se calcula ao final e non afecta ao resultado da expresión o incremento ou decremento
+
+//Operadores lógicos 
+/*
+ Y lógico: && o and 
+ O lógico: || o or 
+ O exclusivo: xor
+ NO lógico: !
+*/
+$a = 1;
+$b = 2;
+$c = 3;
+echo "<br>\$a "; var_dump($a);
+echo "<br>\$b "; var_dump($b);
+echo "<br>\$c "; var_dump($c);
+
+if ( $c>$a && $c>$b ) {
+	echo "<br>$c>$a e ademáis $c>$b";
+}
 
 
 ?>	

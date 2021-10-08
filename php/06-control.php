136e55f67552575eb54adbc8eb8a7dabdf8cd1d9
iespenanovo <correo.iaw@gmail.com>
2021-10-08 10:38:22 +0200

    c├│digo iaw

--- php/06-control.php
+++ php/06-control.php
@@ -4,6 +4,12 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Estruturas de Control</title>
+	<style>
+		table, th, td {
+			border: 1px solid black;
+			text-align: center;
+		}
+	</style>
 </head>
 <body>
 	<h1>Estruturas de Control</h1>
@@ -64,17 +70,101 @@ else
 	
 	$numero=1;
 
-	while ($numero<=8) {
-		echo "<br>$numero";
+	while ($numero<=5) {
+		echo "\n<br>$numero";
 		$numero++;
 	}
 
-	echo "<br>\$numero ten valor $numero";
+	echo "\n<br>\$numero ten valor $numero";
 
 
+	//facemos o mesmo, pero xerando unha lista html desordenada:
 
-
+	echo "\n<ul>";
+	$numero=1;
+	while($numero<=5) {
+		echo "\n\t<li>$numero</li>";
+		$numero++;
+	}
+	echo "\n</ul>";
 ?>	
+	<h3>For</h3>
+<?php
+	echo "\n<ol>";
+	for ($numero=1; $numero <= 5 ; $numero++) { 
+		echo "\n\t<li>Elemento $numero</li>";
+	}
+	echo "\n</ol>";
+
+?>
+	<h3>Foreach</h3>
+<?php
+	$ingresos = array(
+		'xan' => 1550,
+		'feb' => 2200,
+		'mar' => 2800,
+		'abr' => 3000,
+		'mai' => 3200,
+		'xuñ' => 1500
+	);
+
+	//foreach simple , so para obter valores
+	echo "<p>Ingresos primeiro semestre";
+	foreach ($ingresos as $valor) {
+		echo "\n<br>$valor €";
+	}
+
+	//foreach completo , para obter índices e valores
+	$totalIngresos=0;
+	echo "<p>Ingresos primeiro semestre";
+	foreach ($ingresos as $mes => $valor) {
+		echo "\n<br>$mes = $valor € ";
+		$totalIngresos+=$valor;
+		//var_dump($totalIngresos);
+	}
+	echo "\n<br>Total ingresos: $totalIngresos €";
+?>
+
+<table>
+	<caption>Ingresos primeiro semestre</caption>
+	<tr>
+		<th>Mes</th>
+		<th>Ingresos</th>
+	</tr>
+<?php 
+	$totalIngresos=0;
+	foreach ($ingresos as $nomeMes => $ingresosMes) {
+		echo "\n\t<tr>";
+		echo "\n\t\t<td>$nomeMes</td>";
+		echo "\n\t\t<td>$ingresosMes €</td>";
+		echo "\n\t</tr>";
+		$totalIngresos+=$ingresosMes;
+	}
+	echo "\n\t<tr>";
+	echo "\n\t\t<th>Total ingresos</th>";
+	echo "\n\t\t<th>$totalIngresos €</th>";
+	echo "\n\t</tr>";
+	echo "\n</table>";
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </body>
 </html>

bf163c7530543b006eb3dd269fc5fe4a1efc11af
iespenanovo <correo.iaw@gmail.com>
2021-11-02 12:35:23 +0100

    c├│digo iaw

---
+++ php/14-ficheiros.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=	, initial-scale=1.0">
+	<title>Ficheiros en PHP</title>
+</head>
+<body>
+	<h1>Ficheiros en PHP</h1>
+	
+</body>
+</html>

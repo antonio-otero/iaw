2d5b323a45abcf395ddd4fdfd64015e4a6f05527
iespenanovo <correo.iaw@gmail.com>
2022-01-21 11:04:54 +0100

    c├│digo iaw

---
+++ php/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Índice PHP</title>
+</head>
+<body>
+
+	<h1>Índice clase PHP</h1>
+
+	<ul>
+		<li><a href="01-basico.php">01-basico.php</a></li>
+		<li><a href="02-ambito-varialbes.php">02-ambito-varialbes.php</a></li>
+		<li><a href="03-tipos-datos.php">03-tipos-datos.php</a></li>
+		<li><a href="04-practicas.php">04-practicas.php</a></li>
+		<li><a href="05-operadores.php">05-operadores.php</a></li>
+		<li><a href="06-control.php">06-control.php</a></li>
+		<li><a href="07-practicas.php">07-practicas.php</a></li>
+		<li><a href="08-funcions.php">08-funcions.php</a></li>
+		<li><a href="09-datas-horas.php">09-datas-horas.php</a></li>
+		<li><a href="10-calendario.php">10-calendario.php</a></li>
+		<li><a href="11-formularios-get.php">11-formularios-get.php</a></li>
+		<li><a href="12-formularios-post.php">12-formularios-post.php</a></li>
+		<li><a href="13-formularios-validar.php">13-formularios-validar.php</a></li>
+		<li><a href="14-ficheiros.php">14-ficheiros.php</a></li>
+		<li><a href="15-formularios-ficheiro.php">15-formularios-ficheiro.php</a></li>
+		<li><a href="16-ficheiro-taboa.php">16-ficheiro-taboa.php</a></li>
+		<li><a href="17-directorios.php">17-directorios.php</a></li>
+		<li><a href="18-subir-ficheiros.php">18-subir-ficheiros.php</a></li>
+		<li><a href="19-bases-de-datos.php">19-bases-de-datos.php</a></li>
+		<li><a href="20-formularios-base-datos.php">20-formularios-base-datos.php</a></li>
+		<li><a href="21-base-datos-taboa.php">21-base-datos-taboa.php</a></li>
+		<li><a href="22-sesions.php">22-sesions.php</a></li>
+		<li><a href="22-sesions-2.php">22-sesions-2.php</a></li>
+		<li><a href="22-sesions-pechar.php">22-sesions-pechar.php</a></li>
+	</ul>
+	
+</body>
+</html>
